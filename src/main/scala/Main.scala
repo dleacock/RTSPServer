8e2b7fcfebb3b49387762c69c346435754f3db02
David Leacock <david.leacock@gmail.com>
2021-01-12 21:06:55 -0600

    Setup basic server

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,32 @@
-object Main extends App {
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
 
+import scala.concurrent.ExecutionContextExecutor
+import scala.io.StdIn
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+
+    implicit val system: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "system")
+
+    val route = path("test") {
+      get {
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Works</h1>"))
+      }
+    }
+
+    val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
+
+    println("Server online. Press RETURN to stop...")
+    StdIn.readLine()
+
+    bindingFuture
+      .flatMap(bindingFuture => bindingFuture.unbind())(system.executionContext)
+      .onComplete(done => system.terminate())(system.executionContext)
+
+  }
 }
